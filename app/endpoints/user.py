5d4a670c952948573cd5e03536573b53c5cfc357
slegrand <ut5svsxgc@relay.firefox.com>
2022-03-10 08:41:25 -0500

    Add users/rights relationship routes

--- app/endpoints/user.py
+++ app/endpoints/user.py
@@ -117,3 +117,51 @@ def delete_single_user(user_id):
         404 Not found
         500 Internal Server Error
     """
+
+#
+# routes for rights
+#
+@blueprint.route("/<int:user_id>/rights", methods=["POST"])
+@authenticate
+def post_single_unit_rights(user_id):
+    """Creata a new right and associate it with the user
+
+    Returns:
+        201 + Location of the new unit
+        400 Bad Request
+        404 Not found
+        500 Internal Server Error
+    """
+
+@blueprint.route("/<int:user_id>/rights", methods=["GET"])
+@authenticate
+def get_single_unit_rights(user_id):
+    """Retrieve all rights for a user
+
+    Returns:
+        200 OK
+        404 Not found
+        500 Internal Server Error
+    """
+
+@blueprint.route("/<int:user_id>/rights", methods=["PUT"])
+@authenticate
+def put_single_unit_rights(user_id):
+    """Update all rights for a user
+
+    Returns:
+        204 No Content
+        404 Not Found
+        500 Internal Server Error
+    """
+
+@blueprint.route("/<int:user_id>/rights", methods=["DELETE"])
+@authenticate
+def delete_single_unit_rights(user_id):
+    """Delete all rights for a user
+
+    Returns:
+        204 No content
+        404 Not found
+        500 Internal Server Error
+    """
