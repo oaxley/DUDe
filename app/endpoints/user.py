e2079ec2deaa66cdb3e7467dd0720a641a74b654
slegrand <ut5svsxgc@relay.firefox.com>
2022-03-18 21:21:39 -0400

    Implement User partial routes

--- app/endpoints/user.py
+++ app/endpoints/user.py
@@ -15,13 +15,15 @@
 from __future__ import annotations
 from typing import Any, List, Optional
 
-from flask import Blueprint, jsonify, request, url_for
+from flask import Blueprint, request, url_for
 
 from app import app, db
-from app.models import Team, User
+from app.models import (
+    Team, User, Right, UserRight
+)
 
 from app.helpers import (
-    authenticate, Validator, HTTPResponse
+    authenticate, Validator, HTTPResponse, Database
 )
 
 
@@ -60,12 +62,12 @@ def post_user():
     # check if the team exists
     team: Optional[Team] = Team.query.filter_by(id=data['team_id']).first()
     if team is None:
-        return HTTPResponse.error(404, f"Could not find team with ID #{data['team_id']}")
+        return HTTPResponse.error404(data['team_id'], 'Team')
 
     # check if the user exists already or not
     user: Optional[User] = User.query.filter_by(name=data['name'], email=data['email'], team_id=team.id).first()
     if user is not None:
-        return HTTPResponse.error(400, f"User already existing for this team.")
+        return HTTPResponse.error(400, f"User already exists for this Team.")
 
     try:
         user = User(name=data['name'], email=data['email'], team_id=team.id)
@@ -88,6 +90,45 @@ def get_user():
         400 Bad Request
         500 Internal Server Error
     """
+    # retrieve the parameters from the request (or set the default value)
+    try:
+        params = Validator.parameters(request, [('offset', 0), ('limit', app.config['DEFAULT_LIMIT_VALUE'])])
+    except ValueError as e:
+        return HTTPResponse.error(400, str(e))
+
+    # ensure parameters remains positive
+    params['offset'] = abs(params['offset'])
+    params['limit'] = abs(params['limit'])
+
+    if params['limit'] > app.config['MAX_LIMIT_VALUE']:
+        params['limit'] = app.config['MAX_LIMIT_VALUE']
+
+    try:
+        # retrieve all the items between the limits
+        items: List[User] =(db.session
+            .query(User)
+            .order_by(User.id)
+            .filter(User.id >= params['offset'])
+            .limit(params['limit'])
+            .all()
+        )
+
+        result = {
+            "offset": params['offset'],
+            "limit": params['limit'],
+            "users": [{
+                "id": f"{item.id}",
+                "name": item.name,
+                "email": item.email,
+                "team_id": item.team_id
+            } for item in items]
+        }
+
+        return HTTPResponse.ok(result)
+
+    except Exception as e:
+        return HTTPResponse.error(500, str(e))
+
 
 @blueprint.route(ROUTE_1, methods=["PUT"])
 @authenticate
@@ -109,7 +150,16 @@ def delete_user():
         404 Not found
         500 Internal Server Error
     """
+    try:
+        # retrieve all the existing teams
+        teams: List[Team] = Team.query.order_by(Team.id).all()
+        for team in teams:
+            Database.Delete.User(None, team.id)
 
+        return HTTPResponse.noContent()
+
+    except Exception as e:
+        return HTTPResponse.error(500, str(e))
 
 #
 # routes for a single user
@@ -136,7 +186,7 @@ def get_single_user(user_id):
     # lookup for the user
     user: Optional[User] = User.query.filter_by(id=user_id).first()
     if user is None:
-        return HTTPResponse.error(404, f"Could not find user with ID #{user_id}")
+        return HTTPResponse.error404(user_id, 'User')
 
     try:
         return HTTPResponse.ok({
@@ -145,6 +195,7 @@ def get_single_user(user_id):
             'email': user.email,
             'team_id': user.team_id
         })
+
     except Exception as e:
         return HTTPResponse.error(500, str(e))
 
@@ -158,6 +209,26 @@ def put_single_user(user_id):
         404 Not Found
         500 Internal Server Error
     """
+    # lookup for the user
+    user: Optional[User] = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return HTTPResponse.error404(user_id, 'User')
+
+    data = request.get_json() or {}
+    try:
+        for key in data:
+            if key not in [ 'name', 'email', 'team_id' ]:
+                return HTTPResponse.error(400, f"Could not update field '{key}'.")
+
+            setattr(user, key, data[key])
+
+        db.session.add(user)
+        db.session.commit()
+
+        return HTTPResponse.noContent()
+
+    except Exception as e:
+        return HTTPResponse.error(500, str(e))
 
 @blueprint.route(ROUTE_2, methods=["DELETE"])
 @authenticate
@@ -169,6 +240,16 @@ def delete_single_user(user_id):
         404 Not found
         500 Internal Server Error
     """
+    # lookup for the user
+    user: Optional[User] = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return HTTPResponse.error404(user_id, 'User')
+
+    try:
+        return Database.Delete.User(user.id, None)
+
+    except Exception as e:
+        return HTTPResponse.error(500, str(e))
 
 
 #
@@ -180,11 +261,46 @@ def post_single_user_rights(user_id):
     """Create a new right and associate it with the user
 
     Returns:
-        201 + Location of the new unit
+        201 Location of the new right
         400 Bad Request
         404 Not found
         500 Internal Server Error
     """
+    # lookup for the user
+    user: Optional[User] = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return HTTPResponse.error404(user_id, 'User')
+
+    data = request.get_json() or {}
+
+    # check parameters
+    try:
+        Validator.data(data, [ 'name', 'team_id' ])
+    except KeyError as e:
+        return HTTPResponse.error(400, str(e))
+
+    # check if the right already exists for this team
+    right: Optional[Right] = Right.query.filter_by(name=data['name'], team_id=data['team_id']).first()
+    if right:
+        return HTTPResponse.error(400, "Right already exists for this Team.")
+
+    try:
+        # create the new right
+        right = Right(name=data['name'], team_id=data['team_id'])
+
+        db.session.add(right)
+        db.session.commit()
+
+        # create the association user/right
+        rel = UserRight(user_id=user.id, right_id=right.id)
+        db.session.add(rel)
+        db.session.commit()
+
+        return HTTPResponse.location(user.id, url_for('user.get_single_user', user_id=user.id))
+
+    except Exception as e:
+        return HTTPResponse.error(500, str(e))
+
 
 @blueprint.route(ROUTE_3, methods=["GET"])
 @authenticate
@@ -196,6 +312,10 @@ def get_single_user_rights(user_id):
         404 Not found
         500 Internal Server Error
     """
+    # lookup for the user
+    user: Optional[User] = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return HTTPResponse.error404(user_id, 'User')
 
 @blueprint.route(ROUTE_3, methods=["PUT"])
 @authenticate
@@ -207,6 +327,7 @@ def put_single_user_rights(user_id):
         404 Not Found
         500 Internal Server Error
     """
+    return
 
 @blueprint.route(ROUTE_3, methods=["DELETE"])
 @authenticate
@@ -218,3 +339,7 @@ def delete_single_user_rights(user_id):
         404 Not found
         500 Internal Server Error
     """
+    # lookup for the user
+    user: Optional[User] = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return HTTPResponse.error404(user_id, 'User')
